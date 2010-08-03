f6008489725f32cef2a94b2c671cd4b52f3d1c8a
Anatoly Bubenkov <bubenkoff@gmail.com>
2010-08-03 20:43:23 +0000

    add generations

--- src/zojax/personal/space/generations/__init__.py
+++ src/zojax/personal/space/generations/__init__.py
@@ -19,5 +19,5 @@ from zope.app.generations.generations import SchemaManager
 
 schemaManager = SchemaManager(
     minimum_generation=0,
-    generation=0,
+    generation=1,
     package_name='zojax.personal.space.generations')
